8895aaefb57fafff10fcb5849a494828d2d9cfd5
unknown <alexandrialware@gmail.com>
2017-12-23 15:36:21 -0500

    Initial Commit

---
+++ FortuneTellerRedo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortuneTellerRedo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Take in first name and last name and store in all uppercase
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine().ToUpper();
+
+
+            Console.WriteLine("How old are you?");
+
+            Console.WriteLine("What is your favorite ROYGBIV color?");
+
+            Console.WriteLine("How many siblings do you have?");
+
+        }
+    }
+}
